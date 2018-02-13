a2f548861afc008a13e68e935ef4df33e07116d2
Unreal <unreal852@hotmail.fr>
2018-02-14 00:51:25 +0100

    Assembly Info

--- ShutdownIt/Properties/AssemblyInfo.cs
+++ ShutdownIt/Properties/AssemblyInfo.cs
@@ -12,14 +12,14 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShutdownIt")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Unreal852 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 //Pour commencer à générer des applications localisables, définissez
 //<UICulture>CultureUtiliséePourCoder</UICulture> dans votre fichier .csproj
